64770c58595911a8acf898e7d45a3c4eaff074d7
yozora911 <puusuke0323113@icloud.com>
2025-04-26 21:35:29 +0900

    作業

--- Source/CameraController.cpp
+++ Source/CameraController.cpp
@@ -13,8 +13,8 @@ void CameraController::Update(float elapsedTime)
 	float speed = rollSpeed * elapsedTime;
 
 	//スティックの入力値に合わせてX軸とY軸を回転
-	angle.x += ax * speed;
-	angle.y += ay * speed;
+	angle.x += ay * speed;
+	angle.y += ax * speed;
 
 	//カメラ回転値を回転行列に変換
 	DirectX::XMMATRIX Transform = DirectX::XMMatrixRotationRollPitchYaw(angle.x, angle.y, angle.z);
--- Source/SceneGame.cpp
+++ Source/SceneGame.cpp
@@ -52,7 +52,7 @@ void SceneGame::Update(float elapsedTime)
 
 	////カメラコントローラー更新処理
 	DirectX::XMFLOAT3 target = { 0,0,0 };
-	//target.y += 0.5f;
+	target.y += 0.5f;
 	cameraController->SetTarget(target);
 	cameraController->Update(elapsedTime);
 }
@@ -76,27 +76,6 @@ void SceneGame::Render()
 	rc.view = camera.GetView();
 	rc.projection = camera.GetProjection();
 
-	//// ビュー行列
-	//{
-	//	DirectX::XMFLOAT3 eye = { 0, 10, -10 };	// カメラの視点（位置）
-	//	DirectX::XMFLOAT3 focus = { 0, 0, 0 };	// カメラの注視点（ターゲット）
-	//	DirectX::XMFLOAT3 up = { 0, 1, 0 };		// カメラの上方向
-
-	//	DirectX::XMVECTOR Eye = DirectX::XMLoadFloat3(&eye);
-	//	DirectX::XMVECTOR Focus = DirectX::XMLoadFloat3(&focus);
-	//	DirectX::XMVECTOR Up = DirectX::XMLoadFloat3(&up);
-	//	DirectX::XMMATRIX View = DirectX::XMMatrixLookAtLH(Eye, Focus, Up);
-	//	DirectX::XMStoreFloat4x4(&rc.view, View);
-	//}
-	//// プロジェクション行列
-	//{
-	//	float fovY = DirectX::XMConvertToRadians(45);	// 視野角
-	//	float aspectRatio = graphics.GetScreenWidth() / graphics.GetScreenHeight();	// 画面縦横比率
-	//	float nearZ = 0.1f;	// カメラが映し出すの最近距離
-	//	float farZ = 1000.0f;	// カメラが映し出すの最遠距離
-	//	DirectX::XMMATRIX Projection = DirectX::XMMatrixPerspectiveFovLH(fovY, aspectRatio, nearZ, farZ);
-	//	DirectX::XMStoreFloat4x4(&rc.projection, Projection);
-	//}
 
 	// 3Dモデル描画
 	{
